34c7d146b558ce02111589df9c5702968191e5b6
Eileen Kuehn <eileen.kuehn@kit.edu>
2016-11-14 16:18:46 +0100

    added possibility to add environment variables per task within workflow

--- assess_workflows/generic/workflow.py
+++ assess_workflows/generic/workflow.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shlex
 import subprocess
 
@@ -6,12 +7,13 @@ from assess_workflows.generic.structure import Structure
 
 
 class Task(object):
-    def __init__(self, cli_path=None, cmd=None, save=True, use_input=False, **kwargs):
+    def __init__(self, cli_path=None, cmd=None, save=True, use_input=False, env={}, **kwargs):
         self.cli_path = cli_path
         self.cmd = cmd
         self.save = save
         self.use_input = use_input
         self.others = kwargs if kwargs else {}
+        self.env = env
 
     def build_subprocess(self, **kwargs):
         command = "python %s --step %s %s %s %s %s" % (
@@ -72,6 +74,10 @@ class Workflow(object):
         print("Using environment %s" % environment_variables)
         environment = os.environ
         environment.update(environment_variables)
+        environment["PYTHONPATH"] = os.path.abspath(sys.path[0]) + \
+            (':' + os.environ['PYTHONPATH'] if 'PYTHONPATH' in os.environ else '')
         for index, task in enumerate(self._tasks[start:end], start=start):
             print("starting workflow %d of %d" % (index+1, len(self._tasks)))
-            subprocess.check_call(task.build_subprocess(index=index+1), env=environment)
+            current_environment = environment.copy()
+            current_environment.update(task.env)
+            subprocess.check_call(task.build_subprocess(index=index+1), env=current_environment)
