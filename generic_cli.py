a063e10070bda73fa07bdda76007b9630f01c027
Eileen Kuehn <eileen.kuehn@kit.edu>
2017-01-29 13:06:56 +0100

    links now removed when already existent

--- generic_cli.py
+++ generic_cli.py
@@ -141,7 +141,8 @@ def intermediate_as_input(ctx, from_steps, to_steps, file_type):
             try:
                 os.symlink(file_path, structure.input_file_path(step=to_step, file_type=file_type))
             except OSError:
-                pass
+                os.unlink(file_path)
+                os.symlink(file_path, structure.input_file_path(step=to_step, file_type=file_type))
 
 
 @click.command()
